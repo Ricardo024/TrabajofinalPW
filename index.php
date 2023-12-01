6d3412a07914f1670ffca8cf683baa37c1e1472a
Ricardo024 <ricardoland24@gmail.com>
2023-11-30 19:47:37 -0600

    11.archivo index agrego el codigo para funcionamiento

---
+++ index.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sesion</title>
+    <link rel="shortcut icon" href="icon.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<form action="resultado.php" method="post">
+
+ <section class="login">
+        <h5>Iniciar sesión</h5>
+        <input class="controls" type="text" name="nombre_usuario" placeholder="Usuario">
+        <input class="controls" type="password" name="password" required placeholder="Contraseña">
+        <button class="buttons">
+            <span>Ingresar</span></button>
+            <button class="buttons">
+           <div><a href="registro.php">Registrate.</a></div>
+        </button>
+
+    </section>
+
+ </form>
+    
+</body>
+</html>
