504d9cb82aa81449a47bc660a9ccfaa7854216db
Ricardo024 <ricardoland24@gmail.com>
2023-11-30 19:32:58 -0600

    3.archivo BorrarUsuario agrego el codigo para funcionamiento

---
+++ BorrarUsuario.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<link rel="shortcut icon" href="icon.jpg" type="image/x-icon">
+<link rel="stylesheet" href="styledash.css">
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BorrarUsuario</title>
+</head>
+
+<body>
+    <h1 style=" text-align: center;" >Elimina Usuario</h1>
+    <form style=" text-align: center;" method="GET" action="deleteUser.php">
+
+        <input  type="text" name="nombre" placeholder="Nombre del usuario" />
+        <br />
+        <button type="submit">Eliminar usuario</button>
+
+    </form>
+    
+    <h1><a href="dashboard.php">Inicio de listas</a></h1>
+
+</body>
+
+</html>
